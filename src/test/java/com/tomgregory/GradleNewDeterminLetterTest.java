e537498cad1f6bd1d735fd1a508b252853b5babe
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-05-09 17:38:13 +0530

    Improved Unit test generated by RoostGPT
    Using AI Model gpt-4-turbo, for user feedback: add comment to explain the test file

--- src/test/java/com/tomgregory/GradleNewDeterminLetterTest.java
+++ src/test/java/com/tomgregory/GradleNewDeterminLetterTest.java
@@ -5,14 +5,25 @@
 roost_feedback [5/9/2024, 4:33:21 PM]:write tests of all the use cases of the function
 
 roost_feedback [5/9/2024, 4:37:47 PM]:add test for the case if number is less than 0
+
+roost_feedback [5/9/2024, 5:37:29 PM]:add comment to explain the test file
 */
 
 // ********RoostGPT********
+
+/*
+
+roost_feedback [5/9/2024, 4:33:21 PM]:write tests of all the use cases of the function
+
+roost_feedback [5/9/2024, 4:37:47 PM]:add test for the case if number is less than 0
+*/
+
 
 package com.tomgregory;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// This class is designed to test the determinLetter method which grades numbers according to predefined ranges.
 public class GradleNewDeterminLetterTest {
 
     @Test(expected = IllegalArgumentException.class)
@@ -41,28 +52,33 @@ public class GradleNewDeterminLetterTest {
     }
     
     @Test
-    public void testJustBelowCGrade() {
-        assertEquals('D', determinLetter(79));
-    }
-    
-    @Test
     public void testLowestCGrade() {
-        assertEquals('C', determinLetter(80));
+        assertEquals('C', determinLetter(70));
     }
     
     @Test
     public void testHighestCGrade() {
-        assertEquals('C', determinLetter(89));
+        assertEquals('C', determinLetter(79));
     }
     
     @Test
     public void testLowestBGrade() {
-        assertEquals('B', determinLetter(90));
+        assertEquals('B', determinLetter(80));
     }
     
     @Test
     public void testHighestBGrade() {
-        assertEquals('B', determinLetter(99));
+        assertEquals('B', determinLetter(89));
+    }
+    
+    @Test
+    public void testLowestAGrade() {
+        assertEquals('A', determinLetter(90));
+    }
+    
+    @Test
+    public void testHighestAGrade() {
+        assertEquals('A', determinLetter(99));
     }
     
     @Test
@@ -87,7 +103,9 @@ public class GradleNewDeterminLetterTest {
             return 'C';
         } else if (numGrade < 90) {
             return 'B';
-        } else if (numGrade <= 100) {
+        } else if (numGrade < 100) {
+            return 'A';
+        } else if (numGrade == 100) {
             return 'O';
         } else {
             throw new IllegalArgumentException("Grades should not be over 100");
