bf67e800cdcb6b81590fe4077e92869ae2ccfc46
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-05-10 18:04:52 +0530

    small change

--- src/main/java/com/tomgregory/SimpleCalculator.java
+++ src/main/java/com/tomgregory/SimpleCalculator.java
@@ -5,17 +5,5 @@ public class SimpleCalculator{
     public int add(int a,int b){
         return a+b;
     }
-
-    public int multiply(int a,int b){
-        return a*b;
-    }
-
-    public int divide(int a,int b){
-        return a/b;
-    }
-
-    public int newOperation(int a,int b,int c,int o,int i){
-        return a*b+c/o-i;
-    }
     
 }
